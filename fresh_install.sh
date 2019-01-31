4b45d69ac1b15c94a9da9313a792d3fcb7cb74e9
Matthieu Chatelan <m.chatelan@gmail.com>
2019-01-31 16:11:48 +0100

    Update fresh_install.sh
    adding syntax coloring

--- fresh_install.sh
+++ fresh_install.sh
@@ -7,6 +7,7 @@ sudo apt install -y glances vim zsh wget screenfetch tree
 # Install oh-my-zsh and autocomplete themes
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh-syntax-highlighting
 
 
 # Remove old dotfiles and replace them with the new ones
