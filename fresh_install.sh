54d060ffb60dddd9aef89d3eb883d388fbc69575
Matthieu Chatelan <m.chatelan@gmail.com>
2019-04-02 14:08:34 +0200

    Adding unattended upgrades

--- fresh_install.sh
+++ fresh_install.sh
@@ -1,8 +1,12 @@
 # Update the system to the latest version available
 sudo apt update && sudo apt upgrade -y
 
+# Enabling automated upgrades
+sudo mv 50unattended-upgrades /etc/apt/apt.conf.d/
+sudo mv 20auto-upgrades /etc/apt/apt.conf.d/
+
 # Install basic packages
-sudo apt install -y glances vim zsh wget screenfetch tree
+sudo apt install -y glances vim zsh wget screenfetch tree ranger
 
 # Install oh-my-zsh and autocomplete themes
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
