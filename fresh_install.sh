4401fb754207cb246eb01c98e64fe41dca6143f9
Matthieu Chatelan <m.chatelan@gmail.com>
2019-04-05 10:24:51 +0200

    added language pack

--- fresh_install.sh
+++ fresh_install.sh
@@ -6,7 +6,7 @@ sudo mv 50unattended-upgrades /etc/apt/apt.conf.d/
 sudo mv 20auto-upgrades /etc/apt/apt.conf.d/
 
 # Install basic packages
-sudo apt install -y glances vim zsh wget screenfetch tree ranger
+sudo apt install -y glances vim zsh wget screenfetch tree ranger language-pack-en
 
 # Install oh-my-zsh and autocomplete themes
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
