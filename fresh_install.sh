b449a42e890c4a0117bc9a8f55fff2b0c1391579
Matthieu Chatelan <m.chatelan@gmail.com>
2019-12-20 13:35:38 +0100

    Update fresh_install.sh

--- fresh_install.sh
+++ fresh_install.sh
@@ -8,14 +8,20 @@ sudo mv 20auto-upgrades /etc/apt/apt.conf.d/
 # Install basic packages
 sudo apt install -y glances vim zsh wget screenfetch tree ranger language-pack-en
 
-# Install oh-my-zsh and autocomplete themes
+# Required SSH configuration
+sudo mv ca.pub /etc/ssh
+echo TrustedUserCAKeys /etc/ssh/ca.pub | sudo tee -a /etc/ssh/sshd_config
+ssh-keygen -t ecdsa
+sudo systemctl restart sshd
+
+# Install oh-my-zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+# Installing zsh plugins
+echo "Installing zsh plugins..."
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh-syntax-highlighting
 git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
-
-# Place the ca.pub at the correct place
-sudo mv ca.pub /etc/ssh
 
 # Remove old dotfiles and replace them with the new ones
 rm ~/.zshrc
