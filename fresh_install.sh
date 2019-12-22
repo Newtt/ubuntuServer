02d263d6583a74789ecdc9238a2148124e39dacf
Matthieu Chatelan <m.chatelan@gmail.com>
2019-12-22 13:13:08 +0100

    Update fresh_install.sh

--- fresh_install.sh
+++ fresh_install.sh
@@ -27,3 +27,28 @@ git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
 rm ~/.zshrc
 cp dotfiles/.vimrc ~/.vimrc
 cp dotfiles/.zshrc ~/.zshrc
+
+# ZABBIX INSTALLATION
+# Update packet repo
+wget http://repo.zabbix.com/zabbix/4.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_4.4-1%2Bbionic_all.deb
+sudo dpkg -i zabbix-release_4.4-1+bionic_all.deb
+
+# Install the agent
+sudo apt update && sudo apt install zabbix-agent -y
+
+# Remove the downloaded deb file
+rm zabbix-release_4.4-1+bionic_all.deb
+
+# Modify the configuration
+read -p 'Zabbix server IP : ' server_ip
+sudo sed -i "s/127.0.0.1/$server_ip/g" /etc/zabbix/zabbix_agentd.conf
+
+# Enable the service
+sudo systemctl enable zabbix-agent
+sudo systemctl start zabbix-agent
+
+# NTP Setup
+echo 'Servers=0.debian.pool.ntp.org 1.debian.pool.ntp.org 2.debian.pool.ntp.org 3.debian.pool.ntp.org' | sudo tee -a /etc/systemd/timesyncd.conf
+sudo systemctl restart systemd-timesyncd
+
+echo 'done'
