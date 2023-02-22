4429f01b85a7f22320acef6833bcc966046684ab
Matthieu Chatelan <matthieu.chatelan@infoguard.ch>
2023-02-22 07:55:13 +0100

    update all

--- fresh_install.sh
+++ fresh_install.sh
@@ -6,7 +6,7 @@ sudo mv 50unattended-upgrades /etc/apt/apt.conf.d/
 sudo mv 20auto-upgrades /etc/apt/apt.conf.d/
 
 # Install basic packages
-sudo apt install -y vim zsh wget tree language-pack-en
+sudo apt install -y vim zsh wget tree language-pack-en tmux
 
 # Required SSH configuration
 sudo mv ca.pub /etc/ssh
@@ -19,6 +19,5 @@ sudo sed -i "s/#PasswordAuthentication yes/PasswordAuthentication no/" /etc/ssh/
 # NTP Setup
 echo 'Servers=0.debian.pool.ntp.org 1.debian.pool.ntp.org 2.debian.pool.ntp.org 3.debian.pool.ntp.org' | sudo tee -a /etc/systemd/timesyncd.conf
 sudo systemctl restart systemd-timesyncd
-
 
 echo 'done'
--- personalise_shell.sh
+++ personalise_shell.sh
@@ -1,18 +1,15 @@
 # Install oh-my-zsh
-echo "Installing oh-my-zsh..."
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 # Installing zsh plugins
-echo "Installing zsh plugins..."
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # Remove old dotfiles and replace them with the new ones
-echo "Replacing dotfiles..."
 rm ~/.zshrc
 cp dotfiles/.vimrc ~/.vimrc
 cp dotfiles/.zshrc ~/.zshrc
+cp dotfiles/.tmux.conf ~/.tmux.conf
 
 # Install special zsh plugins and themes
-echo "Installing powerlevel10k..."
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
